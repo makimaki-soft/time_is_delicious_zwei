3c2058310c0f1fc745c5e22e938228ab68de681f
shisoromi <over.hrk@gmail.com>
2018-06-17 01:46:31 +0900

    プレイヤーとターン待ち合わせを変形

---
+++ TimeIsDeliciousZwei/Assets/MakiMaki/Scripts/MKExtention.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public static class DictionaryExtensions
+{
+    /// <summary>
+    /// 値を取得、keyがなければデフォルト値を設定し、デフォルト値を取得
+    /// </summary>
+    public static TV GetOrDefault<TK, TV>(this Dictionary<TK, TV> dic, TK key, TV defaultValue = default(TV))
+    {
+        TV result;
+        return dic.TryGetValue(key, out result) ? result : defaultValue;
+    }
+}
--- TimeIsDeliciousZwei/Assets/Scripts/PhaseManager.cs
+++ TimeIsDeliciousZwei/Assets/Scripts/PhaseManager.cs
@@ -60,28 +60,6 @@ public class PhaseManager : MonoBehaviour
     // Dependency
     public ITimeIsDeliciousZwei GameMagager { private get; set; }
 
-    // Notify
-    private Subject<Unit> _phaseSubject = new Subject<Unit>();
-    public void FinishPhase(Phase phase)
-    {
-        _phaseFlag[phase] = true;
-        _phaseSubject.OnNext(Unit.Default);
-    }
-
-    private Subject<int> _playerIdxSubject = new Subject<int>();
-    public void AdvancePlayer(int index)
-    {
-        _playerFlag[index] = true;
-        _playerIdxSubject.OnNext(index);
-    }
-
-    private Subject<int> _turnSubject = new Subject<int>();
-    public void AdvanceTurn(int turn)
-    {
-        _turnFlag[turn] = true;
-        _turnSubject.OnNext(turn);
-    }
-
     public void StartGame()
     {
         var indexList = new List<int>();
@@ -101,22 +79,48 @@ public class PhaseManager : MonoBehaviour
         Observable.FromCoroutine(PhaseControl).Subscribe().AddTo(gameObject);
     }
 
-    private ObservableYieldInstruction<bool> SyncPhase(Phase phase)
-    {
-        return _phaseSubject.StartWith(Unit.Default).Select(_ => _phaseFlag[phase]).Where(b=>b).FirstOrDefault().ToYieldInstruction();
-    }
-
     Dictionary<Phase, bool> _phaseFlag = new Dictionary<Phase, bool>();
     Dictionary<int, bool> _turnFlag = new Dictionary<int, bool>();
     Dictionary<int, bool> _playerFlag = new Dictionary<int, bool>();
 
+    private Subject<Unit> _syncSubject = new Subject<Unit>();
+
+    public void FinishPhase(Phase phase)
+    {
+        _phaseFlag[phase] = true;
+        _syncSubject.OnNext(Unit.Default);
+    }
+
+    public void AdvancePlayer(int index)
+    {
+        _playerFlag[index] = true;
+        _syncSubject.OnNext(Unit.Default);
+    }
+
+    public void AdvanceTurn(int turn)
+    {
+        _turnFlag[turn] = true;
+        _syncSubject.OnNext(Unit.Default);
+    }
+
+    private ObservableYieldInstruction<bool> SyncPhase(Phase phase)
+    {
+        return _syncSubject.StartWith(Unit.Default).Select(_ => _phaseFlag.GetOrDefault(phase)).Where(b=>b).FirstOrDefault().ToYieldInstruction();
+    }
+
+    private ObservableYieldInstruction<bool> SyncPlayer(int index)
+    {
+        return _syncSubject.StartWith(Unit.Default).Select(_ => _playerFlag.GetOrDefault(index)).Where(b => b).FirstOrDefault().ToYieldInstruction();
+    }
+
+    private ObservableYieldInstruction<bool> SyncAction(int count)
+    {
+        return _syncSubject.StartWith(Unit.Default).Select(_ => _turnFlag.GetOrDefault(count)).Where(b => b).FirstOrDefault().ToYieldInstruction();
+    }
+
     IEnumerator PhaseControl()
     {
-        foreach (Phase phase in Enum.GetValues(typeof(Phase)))
-        {
-            _phaseFlag[phase] = false;
-        }
-
+        _phaseFlag.Clear();
         _round.Value = 0;
 
         _phase.Value = Phase.GamePreparation;
@@ -125,69 +129,40 @@ public class PhaseManager : MonoBehaviour
         
         for(_round.Value = 1; /* forever */ ; _round.Value++)
         {
-            foreach (Phase phase in Enum.GetValues(typeof(Phase)))
-            {
-                _phaseFlag[phase] = false;
-            }
+            _phaseFlag.Clear();
 
             _phase.Value = Phase.PlayerAction;
 
-            for (int i = 0; i < _playerIdx.Count; i++)
-            {
-                _playerFlag[i] = false;
-            }
-
+            _playerFlag.Clear();
             foreach (var index in _playerIdx)
             {
-                for (int i = 0; i < _turn.Count; i++)
-                {
-                    _turnFlag[i] = false;
-                }
-
+                _turnFlag.Clear();
                 foreach (var cnt in _turn)
                 {
-
-                    while(!_turnFlag[cnt])
-                    {
-                        yield return _turnSubject.Where(idx => cnt == idx).FirstOrDefault().ToYieldInstruction(); // プレイヤーのアクション実行を待ち合わせ 
-                    }
+                    yield return SyncAction(cnt);
                 }
-
-                while(!_playerFlag[index])
-                {
-                    yield return _playerIdxSubject.Where(idx => index == idx).FirstOrDefault().ToYieldInstruction();
-                }   
+                yield return SyncPlayer(index);
             }
 
             yield return SyncPhase(Phase.PlayerAction);
             
             _phase.Value = Phase.Putrefy;
 
-            // 腐敗トークン処理の待ち合わせ
             yield return SyncPhase(Phase.Putrefy);
 
             _phase.Value = Phase.CashOut;
 
-            for (int i = 0; i < _playerIdx.Count; i++)
-            {
-                _playerFlag[i] = false;
-            }
-
+            _playerFlag.Clear();
             foreach (var index in _playerIdx)
             {
-
-                // 売却を待ち合わせ
-                while (!_playerFlag[index])
-                {
-                    yield return _playerIdxSubject.Where(idx => index == idx).FirstOrDefault().ToYieldInstruction();
-                }
+                yield return SyncPlayer(index);
 
                 if(false)
                 {
                     EndFlag.Value = true;
                 }
             }
-
+            
             yield return SyncPhase(Phase.CashOut);
 
             if (EndFlag.Value)
@@ -197,25 +172,16 @@ public class PhaseManager : MonoBehaviour
 
             _phase.Value = Phase.FoodPreparation;
 
-            for (int i = 0; i < _playerIdx.Count; i++)
-            {
-                _playerFlag[i] = false;
-            }
-
+            _playerFlag.Clear();
             foreach (var index in _playerIdx)
             {
-                // 仕込みを待ち合わせ
-                while (!_playerFlag[index])
-                {
-                    yield return _playerIdxSubject.Where(idx => index == idx).FirstOrDefault().ToYieldInstruction();
-                }
+                yield return SyncPlayer(index);
             }
 
             yield return SyncPhase(Phase.FoodPreparation);
 
             _phase.Value = Phase.Aging;
 
-            // 日数経過を待ち合わせ
             yield return SyncPhase(Phase.Aging);
         }
 
